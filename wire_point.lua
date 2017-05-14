70704a640aada76c5295cead8cceb6681c75143f
GaiGai <henhaodegaigai@gmail.com>
2017-05-13 21:40:00 -0400

    Add files via upload

--- wire_point.lua
+++ wire_point.lua
@@ -10,11 +10,12 @@ function wire_point:init(pos,w,opens)
 end
 
 function wire_point:draw(s,w)
-    local o = self.opens
+    local p = self
+    fill(COLOR3)
     rect(p.x*s,p.y*s,w*3) fill(COLOR2)
     rect(p.x*s,p.y*s,w)
-    for j , ops in pairs(o) do
-        local d = ops.d
+    for k , d in pairs(self.opens) do
+        local d = d*s
         rect(p.x*s+d.x*w,p.y*s+d.y*w,w)
     end
 end
