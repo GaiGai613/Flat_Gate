b0624372a48905b39fb9043b33ae5ea2669c4a38
GaiGai <henhaodegaigai@gmail.com>
2017-05-15 19:59:14 -0400

    Add files via upload

--- not_gate.lua
+++ not_gate.lua
@@ -9,7 +9,7 @@ function not_gate:init(obj)
         local e = obj.editor
         local s = e.size
         self.obj = obj
-        self.update_button_pos(self) -- Add button.
+        self:update_button_pos() -- Add button.
         self.wire = wire_line(vec2(-1,0),vec2(1,0),e,#e.wire.lines+1) -- Wire.
     end
 end
@@ -34,16 +34,15 @@ function not_gate:update()
     end
 end
 
-function not_gate:update_button_pos(s)
-    local s = self or s
-    if not s.obj.editor then return end
-    if not s.button then s.button = flat_ui:add_button(0,0,0,0,color(0,0)) end
-    local ed = s.obj.editor
+function not_gate:update_button_pos()
+    if not self.obj.editor then return end
+    if not self.button then self.button = flat_ui:add_button(0,0,0,0,color(0,0)) end
+    local ed = self.obj.editor
     local cpos = vec2(ed.camera.x,ed.camera.y)
-    local pos = vec2(s.obj.x,s.obj.y)*ed.size-cpos
+    local pos = vec2(self.obj.x,self.obj.y)*ed.size-cpos
     
-    s.button.x,s.button.y = pos:unpack()
-    s.button.width,s.button.height = s.width*ed.size,s.height*ed.size
+    self.button.x,self.button.y = pos:unpack()
+    self.button.width,self.button.height = self.width*ed.size,self.height*ed.size
 end
 
 function not_gate:touched(t)
--- port.lua
+++ port.lua
@@ -5,11 +5,12 @@ function port:init(obj,t,v)
     self.name = "PORT//NOT ABLE TO USE NOW."
     self.value = v or 0 self.gate_id = 100
     self.width,self.height = 1,1
+    self.wire = wire() self.wire.lines = {{s = vec2(0,0),e = vec2(1,0)}}
     if obj then
         local e = obj.editor
         local s = e.size
         self.obj = obj
-        self.update_button_pos(self) -- Add button.
+        self:update_button_pos() -- Add button.
         self.wire = wire_line(vec2(-1,0),vec2(1,0),e,#e.wire.lines+1) -- Wire.
     end
 end
@@ -17,7 +18,7 @@ end
 function port:draw(s,x,y,info)
     local x,y = (x or 0)*s,(y or 0)*s 
     local wx = x if self.type == "OUTPUT" then wx = wx-s end
-
+    
     if self.wire then -- Wire.
         translate(x,y) self.wire:draw(s) translate(-x,-y) 
     end    
@@ -27,16 +28,15 @@ function port:draw(s,x,y,info)
     text(string.sub(self.type,1,1),x,y)
 end
 
-function port:update_button_pos(s)
-    local s = self or s
-    if not s.obj.editor then return end
-    if not s.button then s.button = flat_ui:add_button(0,0,0,0,color(0,0)) end
-    local ed = s.obj.editor
+function port:update_button_pos()
+    if not self.obj.editor then return end
+    if not self.button then self.button = flat_ui:add_button(0,0,0,0,color(0,0)) end
+    local ed = self.obj.editor
     local cpos = vec2(ed.camera.x,ed.camera.y)
-    local pos = vec2(s.obj.x,s.obj.y)*ed.size-cpos
+    local pos = vec2(self.obj.x,self.obj.y)*ed.size-cpos
     
-    s.button.x,s.button.y = pos:unpack()
-    s.button.width,s.button.height = s.width*ed.size,s.height*ed.size
+    self.button.x,self.button.y = pos:unpack()
+    self.button.width,self.button.height = self.width*ed.size,self.height*ed.size
 end
 
 function port:touched(t)
